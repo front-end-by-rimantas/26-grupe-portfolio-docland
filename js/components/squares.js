55f533ea4538c843ba21a5ed32f6d2943f082546
MariusBudreika <mariusbudreika09@gmail.com>
2021-04-28 17:18:49 +0300

    nothing

--- js/components/squares.js
+++ js/components/squares.js
@@ -1,7 +1,7 @@
 // // 2. scroll stebejimas, kai pasirodo elementas ekrane
 
 const squaresDOM = document.querySelectorAll('.box');
-
+console.log(squaresDOM);
 for (const squaresDOM of squaresDOM) {
     addEventListener('scroll', () => {
         const elementTop = squaresDOM.offsetTop;
