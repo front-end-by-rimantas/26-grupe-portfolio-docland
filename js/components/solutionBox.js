fa318965b7fa9f0b6cd401fc50819688d4029f8d
Gytzum <gytzum@gmail.com>
2021-04-29 19:29:23 +0300

    svg-logos added

--- js/components/solutionBox.js
+++ js/components/solutionBox.js
@@ -1,13 +1,14 @@
 function solutionBox(selector, data){
     const solutionDOM = document.querySelector(selector);
     let HTML = '';
-    console.log(data);
+    let num = 0;
     for (const key in data) {
         let item = data[key];
+        num++
         HTML += `
     <div class="solution-box">
-        <div class= "logo-box" style="background-color:${item.logoBg};">
-              <i class="${item.logoName} solution-logo" style=" color:${item.logoColor}"></i>
+        <div class= "logo-box"; style="background-color:${item.logoBg};">
+              <img src="${item.logoName}${num}.svg" class="solution-logo" style="color:${item.logoColor};">
         </div>
     <h3 class=solution-header>${item.headerText}</h3>
     <p class="solution-paragraph">${item.paraText}</p>
--- js/data/solutionData.js
+++ js/data/solutionData.js
@@ -1,6 +1,6 @@
 const solutionData = [
     {  
-    logoName: `fa fa-globe`,  
+    logoName: `../../img/solution/pi`,  
     logoBg: `#DFF5FF`,
     logoColor: `#21BCFF`,
     headerText: `Geo Location Support`,
@@ -8,21 +8,21 @@ const solutionData = [
         
     },
     {
-    logoName: `fa fa-globe`,
+    logoName: `../../img/solution/pi`,
     logoBg: `#FFF3EB`,
     logoColor: `#F2A29E`,
     headerText: `Order Management`,
     paraText: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis risus mi. Ut placerat qua lectus.`
     },
     {
-    logoName: `fa fa-globe`,
+    logoName: `../../img/solution/pi`,
     logoBg: `#DFFFF6`,
     logoColor: `#2DCEA5`,
     headerText: `Related Product`,
     paraText: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis risus mi. Ut placerat qa lectus.`
     },
     {
-    logoName: `fa fa-shopping-cart`,
+    logoName: `../../img/solution/pi`,
     logoBg: `#E9F3FF`,
     logoColor: `#0472FA`,
     headerText: `Guest Checkout`,
