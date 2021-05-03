b7e5f8e47762f5d661a5e4ac8f220112668bc331
minvaitekunas <minvaitekunas@gmail.com>
2021-05-04 00:23:12 +0300

    customer review data

---
+++ js/data/customer-reviews.js
@@ -0,0 +1,61 @@
+const customerReviewsData = {
+    selector: '#customer_reviwes_block',
+    imgPath: 'img/customer-review/',
+    data: [
+        {
+            img: 'sr2.png',
+            review: 
+            `<i class="fa fa-star" aria-hidden="true"></i>
+             <i class="fa fa-star" aria-hidden="true"></i>
+             <i class="fa fa-star" aria-hidden="true"></i>
+             <i class="fa fa-star" aria-hidden="true"></i>
+             <i class="fa fa-star-half-o" aria-hidden="true"></i>`
+            
+            ,
+            name: 'Jennifer Lida',
+            qoute: 'It’s a beautifully written and true-to-life emotions, perspectives, insights, pov’s! This book had me laughing out loud and crying and then just FEELING. All. The. Feels!!! This makes you take a personal survey of your own life.'
+
+        },
+        {
+            img: 'sr11.png',
+            review: 
+            `<i class="fa fa-star" aria-hidden="true"></i>
+             <i class="fa fa-star" aria-hidden="true"></i>
+             <i class="fa fa-star" aria-hidden="true"></i>
+             <i class="fa fa-star" aria-hidden="true"></i>
+             <i class="fa fa-star-half-o" aria-hidden="true"></i>`
+            
+            ,
+            name: 'Michael Clark',
+            qoute: 'It’s a beautifully written and true-to-life emotions, perspectives, insights, pov’s! This book had me laughing out loud and crying and then just FEELING. All. The. Feels!!! This makes you take a personal survey of your own life.'
+
+        },
+        {
+            img: 'sr22.png',
+            review: 
+            `<i class="fa fa-star" aria-hidden="true"></i>
+             <i class="fa fa-star" aria-hidden="true"></i>
+             <i class="fa fa-star" aria-hidden="true"></i>
+             <i class="fa fa-star" aria-hidden="true"></i>
+             <i class="fa fa-star-half-o" aria-hidden="true"></i>`
+            
+            ,
+            name: 'Lida Jennifer',
+            qoute: 'It’s a beautifully written and true-to-life emotions, perspectives, insights, pov’s! This book had me laughing out loud and crying and then just FEELING. All. The. Feels!!! This makes you take a personal survey of your own life.'
+        },
+        {
+             img: 'sr11.png',
+            review: 
+            `<i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star-half-o" aria-hidden="true"></i>`
+        
+        ,
+        name: 'Clark Michael',
+        qoute: 'It’s a beautifully written and true-to-life emotions, perspectives, insights, pov’s! This book had me laughing out loud and crying and then just FEELING. All. The. Feels!!! This makes you take a personal survey of your own life.'
+            
+        }
+    ]
+}
