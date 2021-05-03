535e98cb6e01a0527935cf17ceee881edde4b322
Mantas <mantasu11@gmail.com>
2021-05-03 10:43:46 +0300

    content and animation on JS

---
+++ js/data/featuresData.js
@@ -0,0 +1,34 @@
+const featuresData = [
+    {
+    logoName: `../../svg/featuresSvg/pf`,
+    headerText: `Import / Export`,
+    link: "#",
+    },
+    {
+    logoName: `../../svg/featuresSvg/pf`,
+    headerText: `Multilanguage`,
+    link: "#", 
+    },
+    {
+    logoName: `../../svg/featuresSvg/pf`,
+    headerText: `Shipping`,
+    link: "#",  
+    },
+    {
+    logoName: `../../svg/featuresSvg/pf`,
+    headerText: `Manage Order`,
+    link: "#",  
+    },
+    {
+    logoName: `../../svg/featuresSvg/pf`,
+    headerText: `Reporting`,
+    link: "#",  
+    },
+    {
+    logoName: `../../svg/featuresSvg/pf`,
+    headerText: `Guest Checkout`,
+    link: "#",  
+    }
+];
+
+export{featuresData}
