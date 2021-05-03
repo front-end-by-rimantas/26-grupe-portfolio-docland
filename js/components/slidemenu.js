2491662526f10d82546ad123f25114e26a96c217
MariusBudreika <mariusbudreika09@gmail.com>
2021-05-03 13:10:42 +0300

    close button

--- js/components/slidemenu.js
+++ js/components/slidemenu.js
@@ -1,33 +1,27 @@
 const burgerDOM = document.querySelector('.links i');
 const menuDOM = document.querySelector('.menu');
 const closeDOM = document.querySelector('.close');
+
+
 console.log(menuDOM);
 
 const openSideMenu = () => {
     menuDOM.classList.add('slidemenu');
 }
+
+const appearX = () => {
+    closeDOM.classList.add('xappear');
+}
+
 const closeSideMenu = () => {
     menuDOM.classList.remove('slidemenu');
 }
+
+const disappearX = () => {
+    closeDOM.classList.remove('xappear');
+}
 burgerDOM.addEventListener('click', openSideMenu);
 closeDOM.addEventListener('click', closeSideMenu);
+burgerDOM.addEventListener('click', appearX);
+closeDOM.addEventListener('click', disappearX);
 console.log(burgerDOM);
-
-
-
-
-
-
-
-
-
-// const btnVideo = document.querySelector(".start-using .video-btn");
-// const youtubeModal = document.querySelector(".youtube-modal");
-// const closeBtn = document.querySelector(".youtube-modal .close-btn");
-
-// const toggleOpenVideo = ()=> {
-//     youtubeModal.classList.toggle("youtube-modal-open")
-// }
-
-// btnVideo.addEventListener('click', toggleOpenVideo);
-// closeBtn.addEventListener('click', toggleOpenVideo);
