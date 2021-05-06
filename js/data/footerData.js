bd9404a60ebeb82ff23b1abd8e512f364a519e15
Mantas <mantasu11@gmail.com>
2021-05-06 12:26:31 +0300

    footerData.js made

---
+++ js/data/footerData.js
@@ -0,0 +1,58 @@
+const socialsLogo = [
+    {
+        icon: `facebook`,
+        href: `#`,
+    },
+    {
+        icon: `twitter`,
+        href: `#`,
+    },
+    {
+        icon: `instagram`,
+        href: `#`,
+    },
+    {
+        icon: `linkedin`,
+        href: `#`,
+    },
+];
+const footerFeaturesData = [
+    {
+        name: `timeline review`,
+        link: `#`,
+    },
+    {
+        name: `custom fields`,
+        link: `#`,
+    }, 
+    {
+        name: `custom templates`,
+        link: `#`,
+    }, 
+    {
+        name: `task dependencies`,
+        link: `#`,
+    },
+];
+const footerCompanyData = [
+    {
+        name: `about us`,
+        link: `#`,
+    },
+    {
+        name: `pricing plan`,
+        link: `#`,
+    },
+    {
+        name: `privacy & policy`,
+        link: `#`,
+    },
+    {
+        name: `latest news`,
+        link: `#`,
+    },
+];
+
+export { socialsLogo };
+export { footerFeaturesData };
+export { footerCompanyData };
